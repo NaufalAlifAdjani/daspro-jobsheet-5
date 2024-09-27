18b53b2e16cbbf7ed1f1b87f89cedb4b7ee1fdca
NaufalAlifAdjani <naufalalifadjani12@gmail.com>
2024-09-27 10:22:52 +0700

    tambah file PemilihanBilangan16.java

--- PemilihanBilangan16.java
+++ PemilihanBilangan16.java
@@ -5,7 +5,7 @@ public class PemilihanBilangan16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Masukkan sebuah Angka: ");
+        System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
         if(angka % 2 == 0){
